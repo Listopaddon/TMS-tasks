e862d983ec8d6bcce1dc7b7eb7d490735a17a0be
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2022-10-06 19:53:07 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClass hello = new HelloWorldClass();
+
+            Console.WriteLine(hello.house);
+        }
+    }
+}
